80046e38ae457012caf524bf14d98f7f0ce2445d
Richard Anaya <richard.anaya@gmail.com>
2025-02-16 09:52:27 -0800

    feat: Add tokenizer with English language support and text normalization

---
+++ src/tokenizer.rs
@@ -0,0 +1,10 @@
+use bm25::{DefaultTokenizer, Language, Tokenizer};
+
+pub fn get_tokenizer() -> impl Tokenizer {
+    let tokenizer = DefaultTokenizer::builder()
+        .language_mode(Language::English)
+        .normalization(true) // Normalize unicode (e.g., 'é' -> 'e', '🍕' -> 'pizza', etc.)
+        .stopwords(true) // Remove common words with little meaning (e.g., 'the', 'and', 'of', etc.)
+        .stemming(true) // Reduce words to their root form (e.g., 'running' -> 'run')
+        .build()
+}
