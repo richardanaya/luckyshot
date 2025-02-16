047955ad6903767bb33420bae919b7c1d8e77e5f
Richard Anaya <richard.anaya@gmail.com>
2025-02-16 00:34:41 -0800

    refactor: Implement custom code tokenizer for BM25 embedder

--- src/bm25_embedder.rs
+++ src/bm25_embedder.rs
@@ -1,4 +1,4 @@
-use bm25::{DefaultTokenizer, EmbedderBuilder};
+use bm25::{EmbedderBuilder, Tokenizer};
 
 #[derive(Debug)]
 pub struct Bm25Vector {
@@ -6,8 +6,18 @@ pub struct Bm25Vector {
     pub values: Vec<f32>,
 }
 
+#[derive(Default)]
+struct CodeTokenizer;
+
+// Tokenize on occurrences of "T"
+impl Tokenizer for CodeTokenizer {
+    fn tokenize(&self, input_text: &str) -> Vec<String> {
+        tokenize_code(input_text)
+    }
+}
+
 pub fn create_bm25_vector(text: &str) -> Bm25Vector {
-    let embedder = EmbedderBuilder::<u32, DefaultTokenizer>::with_avgdl(1.0).build();
+    let embedder = EmbedderBuilder::<u32, CodeTokenizer>::with_avgdl(250.0).build();
     let embedding = embedder.embed(text);
     Bm25Vector {
         indices: embedding.indices().cloned().collect(),
