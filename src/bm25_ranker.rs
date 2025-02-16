454effc6f7641fbd802d5b253ca96a5efcdf6401
Richard Anaya <richard.anaya@gmail.com>
2025-02-16 00:00:53 -0800

    refactor: Modify BM25 ranking to use new embedding creation method

--- src/bm25_ranker.rs
+++ src/bm25_ranker.rs
@@ -1,6 +1,21 @@
-use crate::scan::FileEmbedding;
-use bm25::{ScoredDocument, Scorer};
 use crate::bm25_embedder::create_bm25_vector;
+use crate::scan::FileEmbedding;
+use bm25::{Embedding, ScoredDocument, Scorer, TokenEmbedding};
+
+pub fn create_embedding_from_indices_and_values(
+    indices: Vec<u32>,
+    values: Vec<f32>,
+) -> Embedding<usize> {
+    let mut tokensEmbedddings: Vec<TokenEmbedding> = vec![];
+    for (index, value) in indices.iter().zip(values.iter()) {
+        let tokenEmbedding = TokenEmbedding {
+            index: *index,
+            value: *value,
+        };
+        tokensEmbedddings.push(tokenEmbedding);
+    }
+    Embedding(tokensEmbedddings)
+}
 
 pub fn rank_documents(embeddings: &[FileEmbedding], query: &str) -> Vec<ScoredDocument<usize>> {
     // Create scorer and add documents
@@ -8,11 +23,16 @@ pub fn rank_documents(embeddings: &[FileEmbedding], query: &str) -> Vec<ScoredDo
 
     // Add each document to the scorer with its index as ID
     for (i, e) in embeddings.iter().enumerate() {
-        scorer.upsert(&i, e.bm25_vector.clone());
+        let mut emb =
+            create_embedding_from_indices_and_values(e.bm25_indices.clone(), e.bm25_values.clone());
+        scorer.upsert(&i, emb);
     }
 
     // Create query embedding
     let query_embedding = create_bm25_vector(query);
+    let query_indices = query_embedding.0;
+    let query_values = query_embedding.1;
+    let query_embedding = create_embedding_from_indices_and_values(query_indices, query_values);
 
     // Get matches sorted by score
     scorer.matches(&query_embedding)
